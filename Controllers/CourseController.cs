cb24a0efa4c673633e4562677dc82b732512ae8d
tyrae <tyrae@DESKTOP-H7O2QFL>
2023-08-19 13:58:44 +0300

    Add project files.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Details(int? id)
+        {
+            if(id==null)
+            {
+                //return Redirect("/Course/List");
+                return RedirectToAction("List","Course");
+            }
+
+            if (Repository.Courses.FirstOrDefault(c => c.Id == id) == null)
+            {
+                return NotFound();
+            }
+
+            Course? kurs = Repository.GetById(id);
+            return View(kurs);
+        }
+
+        public IActionResult List()
+        {
+            return View(Repository.Courses);
+        }
+    }
+}
